3f4e43910da289141222d67ca0a7144e2604371b
Sophieingersoll <singersoll001@stpaul.k12.mn.us>
2021-03-15 14:21:43 -0500

    slalom poses

--- src/main/java/frc4536/robot/Poses.java
+++ src/main/java/frc4536/robot/Poses.java
@@ -19,7 +19,18 @@ public class Poses {
             CENTER_AUTO_START = new Pose2d(3.1, -2.45, new Rotation2d(1.0, 0.0)),
             CENTER_AUTO_END = new Pose2d(6, -2.45, new Rotation2d(1.0, 0.0)),
             INNER_AUTO_WAYPOINT = new Pose2d(4.08, -4.0, new Rotation2d(-1.0, 0.0)),
-            INNER_AUTO_END = new Pose2d(5.4, -4, new Rotation2d(-6.2, 1.6))
+            INNER_AUTO_END = new Pose2d(5.4, -4, new Rotation2d(-6.2, 1.6)),
+            SLALOM_START = new Pose2d(0.762, 0.762, new Rotation2d(0.0)),
+            SLALOM_WAYPOINT_ONE = new Pose2d(2.286, 0.762, new Rotation2d(-0.785398)),
+            SLALOM_WAYPOINT_TWO = new Pose2d(3.048, 2.286, new Rotation2d(0.0)),
+            SLALOM_WAYPOINT_THREE = new Pose2d(6.35, 2.286, new Rotation2d(0.785398)),
+            SLALOM_WAYPOINT_FOUR = new Pose2d(7.62, 0.762, new Rotation2d(0.0)),
+            SLALOM_WAYPOINT_FIVE = new Pose2d(8.89, 1.524, new Rotation2d(-1.5708)),
+            SLALOM_WAYPOINT_SIX = new Pose2d(7.62, 2.286, new Rotation2d(-1.5708)),
+            SLALOM_WAYPOINT_SEVEN = new Pose2d(6.096, 0.762, new Rotation2d(0.785398)),
+            SLALOM_WAYPOINT_EIGHT = new Pose2d(3.048, 0.762, new Rotation2d(0.0)),
+            SLALOM_WAYPOINT_NINE = new Pose2d(1.524, 2.286, new Rotation2d(-1.5708)),
+            SLALOM_END = new Pose2d(0.762, 2.286, new Rotation2d(0.0))
             ;
 
 }
