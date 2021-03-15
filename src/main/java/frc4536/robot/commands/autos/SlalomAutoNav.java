d44f6fb384070d222588166fe1c47bb43f8a284e
Sophieingersoll <singersoll001@stpaul.k12.mn.us>
2021-03-15 14:45:11 -0500

    Slalom Auto nav

---
+++ src/main/java/frc4536/robot/commands/autos/SlalomAutoNav.java
@@ -0,0 +1,36 @@
+package frc4536.robot.commands.autos;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc4536.robot.Constants;
+import frc4536.robot.Poses;
+import frc4536.robot.commands.VisionToTargetCommand;
+import frc4536.robot.subsystems.DriveTrain;
+import frc4536.robot.subsystems.Intake;
+
+import java.util.ArrayList;
+public class SlalomAutoNav extends SequentialCommandGroup {
+    public SlalomAutoNav(DriveTrain driveTrain, Pose2d initialPose, Trajectory slalomOne, Trajectory slalomTwo, Trajectory slalomThree, Trajectory slalomFour, Trajectory slalomFive, Trajectory slalomSix, Trajectory slalomSeven, Trajectory slalomEight, Trajectory slalomNine, Trajectory slalomTen) {
+    
+        addCommands(
+            driveTrain.scurveTo(slalomOne),
+            driveTrain.scurveTo(slalomTwo),
+            driveTrain.scurveTo(slalomThree),
+            driveTrain.scurveTo(slalomFour),
+            driveTrain.scurveTo(slalomFive),
+            driveTrain.scurveTo(slalomSix),
+            driveTrain.scurveTo(slalomSeven),
+            driveTrain.scurveTo(slalomEight),
+            driveTrain.scurveTo(slalomNine),
+            driveTrain.scurveTo(slalomTen)
+        );
+    }
+
+    @Override
+    public String getName() {
+        return "Slalom Auto";
+    }
+    
+}
